d1ab2376c2cd1b4aac514cb7d5503952d872c2e4
Muhammad Faez Siddiqui <faezsiddiquiintelik@gmail.com>
2026-05-07 15:27:26 +0500

    Remove Next.js build artifacts and configuration files from the admin directory, including manifests, build IDs, and server files, to clean up the project structure.

--- admin/.next/server/chunks/ssr/_next-internal_server_app_admin_drivers_page_actions_0ezjqb9.js
+++
@@ -1,3 +0,0 @@
-module.exports=[12403,(a,b,c)=>{}];
-
-//# sourceMappingURL=_next-internal_server_app_admin_drivers_page_actions_0ezjqb9.js.map
--- admin/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"6L0G/wg03hXxSV4HULg+YKwlRx677NEeUTIXMSR6USU=\"\n}"
